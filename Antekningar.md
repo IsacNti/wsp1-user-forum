895793650678548e6796336aba52ef7ccdd12a42
IsacNti <isac.lindholm@elev.ga.ntig.se>
2023-03-24 09:13:06 +0100

    2023-03-24 första lecktion

--- Antekningar.md
+++ Antekningar.md
@@ -7,3 +7,36 @@ sammansätta login med node forum, fixat css för login, reg och hem
 
 ### Nästa
 Fortsätta med css fixa så att /post /:id fungerar
+
+## 2023-03-21
+
+### Vad gjorde jag sist 
+
+Sist fixade jag till css för login, index och tittade på post samt new
+
+### vad ska jag göra idag 
+
+Fixa sista av css försöka fixa new sen post
+
+### Vad hindrar mig
+
+Har inte så post är till en speciel user new problem med att du inte är inlogad
+
+### Gjort idag
+
+fixat kontrast fixad så måste vara inloggad för att kunna gör ett inlägg samt de väljer din användare
+
+## 2023-03-24
+
+### Vad gjorde jag sist 
+
+Gjorde klart login så den går för personers login vid skappandet av ett inlägg istället för att välja blande 
+
+### vad ska jag göra idag 
+
+Fixa så post fungerar möjligtvis göra så de går edita en post
+
+### Vad hindrar mig
+
+Kommer inte in i post 
+
--- routes/index.js
+++ routes/index.js
@@ -157,16 +157,39 @@ router.get('/', async function (req, res, next) {
 });
 
 router.get('/new', async function (req, res, next) {
-    const [users] = await promisePool.query("SELECT * FROM il05users");
-    res.render('new.njk', {
+    if(req.session.LoggedIn){
+        const [users] = await promisePool.query("SELECT * FROM unusers WHERE name=?", req.session.userId);
+        return res.render('new.njk', {
         title: 'Nytt inlägg',
         users,
     });
+    }
+    /*
+    för att kunna gå in i new utan att behöva vara inloggad
+
+    const [users] = await promisePool.query("SELECT * FROM il05users");
+    res.render('new.njk', {
+        title: 'Nytt inlägg',
+        //users,
+    });
+    */
+   res.redirect('/login')
 });
 
 router.post('/new', async function (req, res, next) {
     const { author, title, content } = req.body;
 
+    let user = await promisePool.query('SELECT * FROM unusers WHERE name = ?', [author]);
+    if (!user) {
+    user = await promisePool.query('INSERT INTO unusers (name) VALUES (?)', [author]);
+    }
+
+    const userId = user.insertId || user[0][0].id;
+    const [rows] = await promisePool.query('INSERT INTO il05forum (authorId, title, content) VALUES (?, ?, ?)', [userId, title, content]);
+    res.redirect('/');
+    
+});
+    /*
     let user = await promisePool.query('SELECT * FROM il05users WHERE name = ?', [author]);
     if (!user) {
         user = await promisePool.query('INSERT INTO il05users (name) VALUES (?)', [author]);
@@ -177,15 +200,17 @@ router.post('/new', async function (req, res, next) {
     res.redirect('/'); // den här raden kan vara bra att kommentera ut för felsökning, du kan då använda tex. res.json({rows}) för att se vad som skickas tillbaka från databasen
 });
 
+*/
+
 // index slut
 
 //postlista
 
 router.get('/postlista', async function (req, res, next) {
     const [rows] = await promisePool.query(`
-    SELECT il05forum.*, il05users.name AS username
+    SELECT il05forum.*, unusers.name AS username
     FROM il05forum
-    JOIN il05users ON il05forum.authorId = il05users.id;`);
+    JOIN unusers ON il05forum.authorId = unusers.id;`);
     res.render('lista.njk', {
         rows: rows,
         title: 'Forum',
@@ -195,9 +220,9 @@ router.get('/postlista', async function (req, res, next) {
 
 router.get('/post/:id', async function (req, res) {
     const [rows] = await promisePool.query(
-        `SELECT il05forum.*, il05users.name AS username
+        `SELECT il05forum.*, unusers.name AS username
         FROM il05forum
-        JOIN il05users ON il05forum.authorId = il05users.id
+        JOIN unusers ON il05forum.authorId = unusers.id
         WHERE il05forum.id = ?;`,
         [req.params.id]
     );
